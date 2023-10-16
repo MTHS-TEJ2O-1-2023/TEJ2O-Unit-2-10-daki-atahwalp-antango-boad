d1383b6516c11e55730ac790df7345f4ffd7e7d5
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-16 13:22:07 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@
  *
  * Created by: Daki A.B
  * Created on: Oct 2023
- * This program ...
+ * This program turns on certain amounts of light depending on the light level
 */
 
-let lightvalue: number = 0
+let lightvalue: number 
 lightvalue = input.lightLevel()
 let neopixelStrip: neopixel.Strip = null
 
@@ -26,43 +26,43 @@ input.onButtonPressed(Button.A, function () {
 }
 
   if (lightvalue >= 52) {
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
     neopixelStrip.show()
 }
-    if (lightvalue >= 104) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-    }
-    if (lightvalue >= 156) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-    }
+  if (lightvalue >= 104) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+}
+  if (lightvalue >= 156) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+}
 
-    if (lightvalue >= 208) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
-        neopixelStrip.show()
-    }
+  if (lightvalue >= 208) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+}
 })
 input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    basic.showIcon(IconNames.Happy)
+  basic.clearScreen()
+  neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
+  basic.showIcon(IconNames.Happy)
 }
 )
